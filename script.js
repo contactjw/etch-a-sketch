facbda6b04d4828c6dc6267e2ce3af9086a3cf45
John West <contactjohnwest@gmail.com>
2022-03-12 07:52:33 -0800

    Add slider to re-adjust size of grid

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
+let gridSize = 16;
 let drawingSection = document.querySelector(".drawing-section");
-drawingSection.style.gridTemplateRows = "repeat(16, 1fr)"
-drawingSection.style.gridTemplateColumns = "repeat(16, 1fr)"
+drawingSection.style.gridTemplateRows = "repeat(" + gridSize + ", 1fr)";
+drawingSection.style.gridTemplateColumns = "repeat(" + gridSize + ", 1fr)";
+for (let i = 0; i < (gridSize * gridSize); i++) {
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "drawing-div");
+    drawingSection.appendChild(newDiv);
+}
+
 
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
-for (let i = 0; i < 256; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "drawing-div");
-    newDiv.style.border = "1px solid black";
-    drawingSection.appendChild(newDiv);
+function clearGrid() {
+    drawingSection.innerHTML = "";
 }
 
-const divs = document.querySelectorAll(".drawing-div");
+function changeGrid(size) {
+    clearGrid();
+    drawingSection.style.gridTemplateRows = "repeat(" + size + ", 1fr)";
+    drawingSection.style.gridTemplateColumns = "repeat(" + size + ", 1fr)";
 
-for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "drawing-div");
+        drawingSection.appendChild(newDiv);
+        divs = document.querySelectorAll(".drawing-div");
+    }
+
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        divs[i].addEventListener("mouseover", changeBlack);
+        divs[i].addEventListener("mousedown", changeBlack);
+    }
+
+}
+
+let divs = document.querySelectorAll(".drawing-div");
+
+for (let i = 0; i < (gridSize * gridSize); i++) {
     divs[i].addEventListener("mouseover", changeBlack);
     divs[i].addEventListener("mousedown", changeBlack);
 }
+
+
 
 function changeBlack(e) {
     if (e.type === "mouseover" && !mouseDown)
@@ -28,10 +53,28 @@ function changeBlack(e) {
     e.target.style.backgroundColor = "black";
 }
 
+
+
 const clearButton = document.querySelector(".clear-mode");
 clearButton.addEventListener("click", () => {
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
         divs[i].style.backgroundColor = "white";
     }
 })
 
+
+// slider js taken from w3 schools example sliders
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value + "x" + slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value + "x" + slider.value;
+    gridSize = this.value;
+    changeGrid(this.value);
+}
+
+// window.onload = () => {
+//     setupGrid(gridSize);
+// }
