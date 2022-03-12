4ff6f85f5ef3658d98dbf7359aa5c8ba139c130e
John West <contactjohnwest@gmail.com>
2022-03-12 06:55:08 -0800

    Add logic for working click and drag drawing

--- script.js
+++ script.js
@@ -2,8 +2,31 @@ let drawingSection = document.querySelector(".drawing-section");
 drawingSection.style.gridTemplateRows = "repeat(16, 1fr)"
 drawingSection.style.gridTemplateColumns = "repeat(16, 1fr)"
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 for (let i = 0; i < 256; i++) {
     let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "drawing-div");
     newDiv.style.border = "1px solid black";
     drawingSection.appendChild(newDiv);
 }
+
+const divs = document.querySelectorAll(".drawing-div");
+
+for (let i = 0; i < 256; i++) {
+    divs[i].addEventListener("mouseover", changeBlack);
+    divs[i].addEventListener("mousedown", changeBlack);
+}
+
+function changeBlack(e) {
+    if (e.type === "mouseover" && !mouseDown)
+        return;
+    
+    //if they're both active, change the color to black.
+    e.target.style.backgroundColor = "black";
+}
+
+
+
