97f9d2d5ee415370c6c3317ac2e66953c1915e44
John West <contactjohnwest@gmail.com>
2022-03-11 19:42:34 -0800

    Add JavaScript logic to create squares

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
- 
+ for (let i = 0; i < 10; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.style.width = "10px";
+    newDiv.style.height = "10px";
+    newDiv.style.border = "1px solid black";
+    document.querySelector(".drawing-section").appendChild(newDiv);
+}
+
